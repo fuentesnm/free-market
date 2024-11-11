d31d1ac02a3b0eef8a27513426420c2e2a666b6a
Nicolas Fuentes <nicolas_fuentes@hotmail.com>
2024-11-10 21:28:27 -0300

    firebase.js updated

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,12 +2,12 @@ import  { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB8ps4GVwPai46Npris5czgVKOzEN3GEAY",
-    authDomain: "free-market-f7cfb.firebaseapp.com",
-    projectId: "free-market-f7cfb",
-    storageBucket: "free-market-f7cfb.firebasestorage.app",
-    messagingSenderId: "435528590467",
-    appId: "1:435528590467:web:033a4b1a11c4ca3182e7b6"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID
   };
 
 const app = initializeApp(firebaseConfig);
